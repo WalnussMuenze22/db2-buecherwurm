426ce77fd8dba0d8e83d40de9e1105446c992158
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-05-12 12:34:22 +0000

    Replaced delimiter in View.sql

--- SQL Files/View.sql
+++ SQL Files/View.sql
@@ -14,7 +14,7 @@ SELECT Artikel.ArtikelID AS ArtikelID,
        AnzahlVerkauft,
        AnzahlSeiten,
        Verlag.Name AS Verlag,
-       LISTAGG(Autor.Name, '||') WITHIN GROUP (ORDER BY Autor.Name) AS AutorenListe
+       LISTAGG(Autor.Name, '#') WITHIN GROUP (ORDER BY Autor.Name) AS AutorenListe
 FROM Artikel
 JOIN Buch ON (Artikel.ArtikelID = Buch.ArtikelID)
 JOIN Verlag ON (Artikel.VerlagID = Verlag.VerlagID)
