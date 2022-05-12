6366ffaf6c97e3f7bec126f412fd8d70981bc880
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-05-12 09:42:33 +0000

    Fehler in der View behoben

--- SQL Files/View.sql
+++ SQL Files/View.sql
@@ -1,8 +1,37 @@
+DROP VIEW BuchInformationen;
+
 CREATE VIEW BuchInformationen AS
-SELECT Titel, Preis, Steuersatz, Beschreibung, ISBN, Erscheinungsdatum, Sprache, AnzahlVerfuegbar, MindestBestand, AnzahlVerkauft, AnzahlSeiten, Verlag.Name as Verlag, Autor.Name as Autor
+SELECT Artikel.ArtikelID AS ArtikelID,
+       Titel,
+       Preis,
+       Steuersatz,
+       Beschreibung,
+       ISBN,
+       Erscheinungsdatum,
+       Sprache,
+       AnzahlVerfuegbar,
+       MindestBestand,
+       AnzahlVerkauft,
+       AnzahlSeiten,
+       Verlag.Name AS Verlag,
+       LISTAGG(Autor.Name, '||') WITHIN GROUP (ORDER BY Autor.Name) AS AutorenListe
 FROM Artikel
 JOIN Buch ON (Artikel.ArtikelID = Buch.ArtikelID)
 JOIN Verlag ON (Artikel.VerlagID = Verlag.VerlagID)
-JOIN AutorArtikelAssoziation ON (AutorArtikelAssoziation.ArtikelID = Artikel.ArtikelID)
-JOIN Autor ON (AutorArtikelAssoziation.AutorID = Autor.AutorID)
+JOIN AutorArtikelAssoziation ON Artikel.ArtikelID = AutorArtikelAssoziation.ArtikelID
+JOIN Autor ON AutorArtikelAssoziation.AutorID = Autor.AutorID
+GROUP BY Artikel.ArtikelID,
+         Titel,
+         Preis,
+         Steuersatz,
+         Beschreibung,
+         ISBN,
+         Erscheinungsdatum,
+         Sprache,
+         AnzahlVerfuegbar,
+         MindestBestand,
+         AnzahlVerkauft,
+         AnzahlSeiten,
+         Verlag.Name
+ORDER BY (Artikel.ArtikelID) ASC
 ;
