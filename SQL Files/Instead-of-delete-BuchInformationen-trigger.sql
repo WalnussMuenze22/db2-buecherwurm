a87d24f58b352353d5280fdf58e47a5a1c862c7f
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-05-12 18:00:59 +0000

    Upload New File

---
+++ SQL Files/Instead-of-delete-BuchInformationen-trigger.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE TRIGGER BuchInformationenInsteadOfDelete
+    INSTEAD OF DELETE ON BuchInformationen
+    FOR EACH ROW
+BEGIN
+    raise_application_error(-20001, 'Deleting entries from BuchInformationen is not allowed!');
+    /*
+    -- Löscht den Eintrag aus der Tabelle Buch
+    DELETE FROM Buch
+    WHERE Buch.ArtikelID = :OLD.ArtikelID
+    ;
+    
+    -- Löscht die Assoziation zum Autor
+    DELETE FROM AutorArtikelAssoziation
+    WHERE AutorArtikelAssoziation.ArtikelID = :OLD.ArtikelID
+    ;
+    
+    -- Löscht den Eintrag aus der Tabelle Artikel
+    DELETE FROM Artikel
+    WHERE Artikel.artikelID = :OLD.ArtikelID
+    ;
+    */
+END;
