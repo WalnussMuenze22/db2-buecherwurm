b0b54ada3bfc99084ba2c90b499fa083ea85387c
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-05-13 13:17:37 +0000

    Einfacher Satz Testdaten für Buchinformationen

---
+++ SQL Files/TestDaten.sql
@@ -0,0 +1,61 @@
+INSERT INTO BuchInformationen (
+    Titel,
+    Preis,
+    Steuersatz,
+    Beschreibung,
+    ISBN,
+    ErscheinungsDatum,
+    Sprache,
+    AnzahlVerfuegbar,
+    Mindestbestand,
+    AnzahlVerkauft,
+    AnzahlSeiten,
+    Verlag,
+    AutorenListe
+) VALUES (
+    'Die Känguru-Chroniken',
+    10.99,
+    7,
+    'Frisch Frech und völlig Absurd',
+    9783548372570,
+    DATE '2009-03-11',
+    'DE',
+    10,
+    8,
+    0,
+    272,
+    'Ullstein',
+    'Marc-Uwe Kling'
+);
+
+
+INSERT INTO BuchInformationen (
+    Titel,
+    Preis,
+    Steuersatz,
+    Beschreibung,
+    ISBN,
+    ErscheinungsDatum,
+    Sprache,
+    AnzahlVerfuegbar,
+    Mindestbestand,
+    AnzahlVerkauft,
+    AnzahlSeiten,
+    Verlag,
+    AutorenListe
+) VALUES (
+    'Die Känguru-Comics 1: Also ICH könnte das besser',
+    22.00,
+    7,
+    'Das Känguru und Marc-Uwe kommentieren den alltäglichen Wahnsinn auf ZEIT online – und zwar als Comicfiguren!',
+    9783551728289,
+    DATE '2022-03-20',
+    'DE',
+    24,
+    20,
+    0,
+    224,
+    'Carlsen',
+    'Marc-Uwe Kling#Bernd Kissel'
+);
+
