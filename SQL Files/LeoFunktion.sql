560d26562a8d4fbc3cb60783fe9de8c2ff29ea07
leopetersberg <leo@leopebe.de>
2022-05-13 11:45:17 +0200

    .

--- SQL Files/LeoFunktion.sql
+++ SQL Files/LeoFunktion.sql
@@ -1,14 +1,9 @@
--- Oracle-Syntax
---PLSQL procedure for sending emails to all acccounts where last Login is older than a half year
-CREATE or replace procedure send_emails AS
-DECLARE
+create procedure send_emails AS
     CURSOR accounts_cursor is (
-        SELECT DISTINCT ACCOUNT.EMAIL, ADRESSE.NACHNAME
+        SELECT ACCOUNT.EMAIL
         FROM Account
-        join KUNDE on ACCOUNT.ACCOUNTID = KUNDE.ACCOUNTID
-        JOIN ADRESSE on KUNDE.KUNDENID = ADRESSE.KUNDENID
-        WHERE TO_DATE(LETZTERLOGIN) < TO_DATE(sysdate) - 182
-        AND TO_DATE(LETZTERLOGIN) > TO_DATE(sysdate) - 183
+        WHERE (TO_DATE(sysdate) - cast(LETZTERLOGIN as date) )> 182
+        AND   TO_DATE(sysdate) - cast(LETZTERLOGIN as date) < 500
     );
     account_name ADRESSE.NACHNAME%type;
     account_email ACCOUNT.EMAIL%type;
@@ -16,17 +11,18 @@ DECLARE
 Begin
     open accounts_cursor;
     Loop
-        FETCH accounts_cursor into account_name, account_email;
+        FETCH accounts_cursor into account_email;
         EXIT when accounts_cursor%notfound;
-        l_mail_conn := UTL_SMTP.open_connection('web13.alfahosting-server.de', 25);
-        UTL_SMTP.helo(l_mail_conn, 'web13.alfahosting-server.de');
-        UTL_SMTP.mail(l_mail_conn, 'buecherwurm@leopebe.de');
-        UTL_SMTP.rcpt(l_mail_conn, account_email);
-        UTL_SMTP.data(l_mail_conn, 'Dies ist ein Test.' || UTL_TCP.crlf || UTL_TCP.crlf);
-        UTL_SMTP.quit(l_mail_conn);
+        SELECT ADRESSE.NACHNAME INTO account_name FROM ADRESSE Join Bestellung on ADRESSE.ADRESSEID = BESTELLUNG.RECHNUNGSADRESSEID;
+
+        --E-Mail senden vom TH Server nicht möglich, aufgrund Berechtigungen: ORA-24247: Netzwerkzugriff von Access Control-Liste (ACL) abgelehnt
+        --l_mail_conn := UTL_SMTP.open_connection('web13.alfahosting-server.de', 25);
+        --UTL_SMTP.helo(l_mail_conn, 'web13.alfahosting-server.de');
+        --UTL_SMTP.mail(l_mail_conn, 'buecherwurm@leopebe.de');
+        --UTL_SMTP.rcpt(l_mail_conn, account_email);
+        --UTL_SMTP.data(l_mail_conn, 'Dies ist ein Test.' || UTL_TCP.crlf || UTL_TCP.crlf);
+        --UTL_SMTP.quit(l_mail_conn);
     end loop;
     close accounts_cursor;
 end;
-
-
--- Noch nicht fertig
+/
