07834e715b02e61d301e565211e4347523f5c84b
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-05-15 13:38:11 +0000

    Update SQL Files/JesajaFunktion.sql

--- SQL Files/JesajaFunktion.sql
+++ SQL Files/JesajaFunktion.sql
@@ -1,6 +1,6 @@
 -- Autor: Jesaja Storm
 
-CREATE OR REPLACE PROCEDURE adjust_minimum_Stock
+CREATE OR REPLACE PROCEDURE adjust_minimum_stock
 IS
     -- Der Wert für MindestBestand in Prozent gemessen am gesamt Verkauf im letzten Monat
     v_minBestandProzent NUMBER := 40;
@@ -22,8 +22,8 @@ BEGIN
         WHERE (
             Bestellposition.ArtikelID = v_ArtikelID
             AND Datum BETWEEN ADD_MONTHS(TRUNC(SYSDATE, 'mm'), -1) and LAST_DAY(ADD_MONTHS(TRUNC(SYSDATE, 'mm'), -1))
-        )
-        ;
+            AND Bestellung.Status IN ('offen', 'versendet', 'zugestellt')
+        );
         
         UPDATE Artikel
         SET Artikel.MindestBestand = ROUND(v_minBestandProzent / 100 * v_stueckVerkauftLetzterMonat, 0)
