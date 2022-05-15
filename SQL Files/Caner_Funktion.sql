c323d82f44c95d6d0c2b0a4794dc1db7f3507f22
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-05-15 13:05:27 +0000

    Update SQL Files/Create-Table-Praktikum-Modell.sql, SQL Files/Caner_Funktion.sql Deleted Caner_Trigger.sql

--- SQL Files/Caner_Funktion.sql
+++ SQL Files/Caner_Funktion.sql
@@ -1,3 +1,8 @@
+CREATE SEQUENCE BESTELL_SEQ
+START WITH 1
+INCREMENT BY 1
+MINVALUE 1
+;
 
 -- Funktion createSurrogateKey
 CREATE OR REPLACE Function createSurrogateKey
@@ -8,3 +13,13 @@ BEGIN
 surrogateKey:= Sysdate || ' - ' || BESTELL_SEQ.nextval;
 RETURN surrogateKey;
 END createSurrogateKey;
+/
+
+-- Setzt den generierten Primärschlüssel in die Tabelle Bestellung ein
+CREATE OR REPLACE TRIGGER insertGeneratedPrimaryKeyIntoBestellung
+BEFORE INSERT ON Bestellung
+FOR EACH ROW
+DECLARE
+BEGIN
+    :NEW.BestellungID := createSurrogateKey;
+END;
