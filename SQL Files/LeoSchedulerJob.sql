c7b60346da12ef26f48121f9afeb09b4b375432a
leopetersberg <leo@leopebe.de>
2022-05-15 14:30:46 +0200

    .

--- SQL Files/LeoSchedulerJob.sql
+++ SQL Files/LeoSchedulerJob.sql
@@ -3,7 +3,7 @@ BEGIN
     --DBMS_SCHEDULER.DROP_JOB ('send_emails_every_day');
     
     DBMS_SCHEDULER.CREATE_JOB (
-        comments           =>  'versendet jeden Tag E-Mails an Accounts, die ein halbes Jahr cnith eingelogggt waren.',
+        comments           =>  'Versendet jeden Tag E-Mails an Accounts, die ein halbes Jahr nicht eingelogggt waren.',
         
         start_date         =>   SYSTIMESTAMP,
         end_date           =>   NULL,
