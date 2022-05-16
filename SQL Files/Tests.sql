db452997e2c91f99e4342ee590ba1a48d6860ac3
leopetersberg <leo@leopebe.de>
2022-05-16 03:29:37 +0200

    .

--- SQL Files/Tests.sql
+++ SQL Files/Tests.sql
@@ -292,20 +292,29 @@ BEGIN
 
 
     --archive_deleted_account Test 1:
-    SELECT * FROM Account;
-    SELECT * FROM LogAccountActive;
     UPDATE Account SET aktiv = 0 WHERE accountid = 2;
-    SELECT * FROM Account;
-    SELECT * FROM LogAccountActive;
+    SELECT count(*) 
+    INTO v_test_num1
+    FROM LogAccountActive;
+
+    IF (v_test_num1 = 1) THEN
+        DBMS_OUTPUT.PUT_LINE('archive_deleted_account Test2 passed');
+    ELSE
+        raise_application_error(-20123, 'archive_deleted_account Test2 failed!');
+    END IF;
 
     --archive_deleted_account Test 2:
-    SELECT * FROM Account;
-    SELECT * FROM LogAccountActive;
-    UPDATE AccountSET aktiv = 1 WHERE accountid = 2;
-    SELECT * FROM Account;
-    SELECT * FROM LogAccountActive;
+    UPDATE Account SET aktiv = 1 WHERE accountid = 2;
+    SELECT COUNT(*) 
+    INTO v_test_num1
+    FROM LogAccountActive;
 
 
+    IF (v_test_num1 = 1) THEN
+        DBMS_OUTPUT.PUT_LINE('archive_deleted_account Test2 passed');
+    ELSE
+        raise_application_error(-20123, 'archive_deleted_account Test2 failed!');
+    END IF;
 
 
 END;
