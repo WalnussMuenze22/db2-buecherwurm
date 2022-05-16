ad08f683875d16395184af4f4c1fc3b2cad3472d
Dennis Kliewer <dennis.kliewer@smail.th-koeln.de>
2022-05-16 00:27:12 +0000

    Update Trigger-Archive-Deleted-Account final

--- SQL Files/Trigger-Archive-Deleted-Account.sql
+++ SQL Files/Trigger-Archive-Deleted-Account.sql
@@ -1,16 +1,43 @@
 -- Archive Deleted Account
 
-CREATE OR REPLACE TRIGGER archive_deleted_account
-	INSTEAD OF DELETE
+create or replace TRIGGER archive_deleted_account
+	AFTER UPDATE
+    OF Aktiv
 	ON Account 
 	FOR EACH ROW
 DECLARE
-	v_AccountId NUMBER;
+    v_Aktiv NUMBER := 0;
 BEGIN
-		UPDATE Account 
-		SET Aktiv=0 
-		WHERE ACCOUNTID = :old.ACCOUNTID 
-		RETURNING ACCOUNTID INTO v_AccountId;
-		
-		INSERT INTO LogAccountActive(ACCOUNTID,Datum,Aktiv) VALUES (v_AccountId, SYSTIMESTAMP,0);
+        IF :new.Aktiv = 0 then
+            v_Aktiv := 1;
+        ELSE
+            v_Aktiv := 0;
+        end if;    
+
+		INSERT INTO LogAccountActive(ACCOUNTID,Datum,Aktiv) VALUES (:old.AccountID, SYSTIMESTAMP,:new.Aktiv);
 END;
+
+--Test
+SELECT *
+FROM Account;
+SELECT *
+FROM LogAccountActive;
+UPDATE Account
+SET aktiv = 0
+WHERE accountid = 2;
+SELECT *
+FROM Account;
+SELECT *
+FROM LogAccountActive;
+
+SELECT *
+FROM Account;
+SELECT *
+FROM LogAccountActive;
+UPDATE Account
+SET aktiv = 1
+WHERE accountid = 2;
+SELECT *
+FROM Account;
+SELECT *
+FROM LogAccountActive;
