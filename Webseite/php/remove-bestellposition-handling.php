852c9666dbe3f20950853121607d5b70455d4a09
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-06-08 18:04:27 +0000

    Fixed bug

--- Webseite/php/remove-bestellposition-handling.php
+++ Webseite/php/remove-bestellposition-handling.php
@@ -1,4 +1,4 @@
-<?
+<?php
     require_once($_SERVER["DOCUMENT_ROOT"] . "/php/WarenkorbFunctions.php");
     
     $bestellpositionId = filter_input(INPUT_POST, "bestellpositionId", FILTER_SANITIZE_NUMBER_INT);
