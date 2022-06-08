e65c93d88e3b5a05b715f37c2b725628eb212ed8
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-06-08 18:03:01 +0000

    Added remove-bestellposition-handling.php

---
+++ Webseite/php/remove-bestellposition-handling.php
@@ -0,0 +1,9 @@
+<?
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/php/WarenkorbFunctions.php");
+    
+    $bestellpositionId = filter_input(INPUT_POST, "bestellpositionId", FILTER_SANITIZE_NUMBER_INT);
+    WarenKorbFunctions::deleteItemFromWarenkorb($bestellpositionId);
+    
+    header("Location: /cart.php");
+    exit();
+?>
