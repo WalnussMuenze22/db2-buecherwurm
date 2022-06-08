e9d64123c47e69433d0ca73b80fbf644fa50cef0
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-06-08 13:48:17 +0000

    Simplified switchPages.js

--- Webseite/scripts/switchPages.js
+++ Webseite/scripts/switchPages.js
@@ -1,26 +1,17 @@
 function openLogin(){
-    var temp2 = document.getElementById("login");
-    temp2.classList.add("Freed");
-    var temp3 = document.getElementById("register");
-    temp3.classList.remove("Freed");
-    var temp4 = document.getElementById("account");
-    temp4.classList.remove("Freed");
+    document.getElementById("login").classList.add("Freed");
+    document.getElementById("register").classList.remove("Freed");
+    document.getElementById("account").classList.remove("Freed");
 }
 
 function openRegister(){
-    var temp2 = document.getElementById("register");
-    temp2.classList.add("Freed");
-    var temp3 = document.getElementById("login");
-    temp3.classList.remove("Freed");
-    var temp4 = document.getElementById("account");
-    temp4.classList.remove("Freed");
+    document.getElementById("login").classList.remove("Freed");
+    document.getElementById("register").classList.add("Freed");
+    document.getElementById("account").classList.remove("Freed");
 }
 
 function openAccount(){
-    var temp2 = document.getElementById("login");
-    temp2.classList.remove("Freed");
-    var temp3 = document.getElementById("register");
-    temp3.classList.remove("Freed");
-    var temp4 = document.getElementById("account");
-    temp4.classList.add("Freed");
+    document.getElementById("login").classList.remove("Freed");
+    document.getElementById("register").classList.remove("Freed");
+    document.getElementById("account").classList.add("Freed");
 }
