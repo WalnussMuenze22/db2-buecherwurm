0d365bbd8cb940c600f037f966d3b67dece5a3ce
leopetersberg <leo@leopebe.de>
2022-06-08 15:09:23 +0200

    Klassen hinzugefügt

--- Webseite/php/UserFunctions.php
+++ Webseite/php/UserFunctions.php
@@ -1,88 +1,91 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "/db2-buecherwurm/Webseite/php/databaseConnection.php" ;  
-
-/**
- * It takes a connection, an email and a password and returns true if the user is logged in and false
- * if not.
- * 
- * @param email the email address of the user
- * @param password y$/X.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q
- * 
- * @return true if the login was successful and false if it was not.
- */
-function login( $email, $password) {
-    $passwordHash = md5($password);
-    $stmt = oci_parse( DatabaseConnection::getDatabaseConnection(), "SELECT KundenID 
+require_once $_SERVER["DOCUMENT_ROOT"] . "/db2-buecherwurm/Webseite/php/databaseConnection.php";
+class UserFunctions
+{
+    /**
+     * It takes a connection, an email and a password and returns true if the user is logged in and false
+     * if not.
+     * 
+     * @param email the email address of the user
+     * @param password y$/X.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q.Q
+     * 
+     * @return true if the login was successful and false if it was not.
+     */
+    static function login($email, $password)
+    {
+        $passwordHash = md5($password);
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT KundenID 
                                 FROM Kunde
                                 JOIN Account on Kunde.AccountID = Account.AccountID
                                 WHERE Email = :email AND PasswortHash = :password");
-    oci_bind_by_name($stmt, ':email', $email);
-    oci_bind_by_name($stmt, ':password', $passwordHash);
-    oci_execute($stmt);
-    $row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS);
-    if ($row) {
-        // Erstelle eine Session mit dem KundenID
-        session_start();
-        $userID = $row['KundenID'];
-        $_SESSION['userID'] = $userID;
-        //Setzte LastLogin auf aktuelle Zeit
-        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "UPDATE Account SET LastLogin = SYSDATE WHERE AccountID = (SELECT AccountID FROM Kunde WHERE KundenID = :userID)");
-        oci_bind_by_name($stmt, ':userID', $userID);
+        oci_bind_by_name($stmt, ':email', $email);
+        oci_bind_by_name($stmt, ':password', $passwordHash);
         oci_execute($stmt);
-        return true;
-    } else {
-        return false;
+        $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);
+        if ($row) {
+            // Erstelle eine Session mit dem KundenID
+            session_start();
+            $userID = $row['KundenID'];
+            $_SESSION['userID'] = $userID;
+            //Setzte LastLogin auf aktuelle Zeit
+            $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "UPDATE Account SET LastLogin = SYSDATE WHERE AccountID = (SELECT AccountID FROM Kunde WHERE KundenID = :userID)");
+            oci_bind_by_name($stmt, ':userID', $userID);
+            oci_execute($stmt);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * It destroys the session.
+     * 
+     */
+    static function logout()
+    {
+        session_destroy();
+    }
+
+
+    /**
+     * It takes a connection, an email and a password and inserts a new account, customer and saves the
+     * customerID in the session.
+     * 
+     * @param email test@test.de
+     * @param password the password the user entered
+     */
+    static function register($email, $password)
+    {
+        $passwordHash = md5($password);
+        //Account anlegen
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "INSERT INTO Account (PasswortHash,  EMail, AccountTyp, LetzterLogin, Aktiv) VALUES (:passwordHash, :email, 'Kunde', SYSDATE, '1')");
+        oci_bind_by_name($stmt, ':passwordHash', $passwordHash);
+        oci_bind_by_name($stmt, ':email', $email);
+        oci_execute($stmt);
+
+        //AccountID ermitteln
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT AccountID FROM Account WHERE EMail = :email");
+        oci_bind_by_name($stmt, ':email', $email);
+        oci_execute($stmt);
+        $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);
+        $accountID = $row['AccountID'];
+
+        //Kunde anlegen
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "INSERT INTO Kunde (AccountID) VALUES (:accountID)");
+        oci_bind_by_name($stmt, ':accountID', $accountID);
+        oci_execute($stmt);
+
+        //KundenID ermitteln
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT KundenID FROM Kunde WHERE AccountID = :accountID");
+        oci_bind_by_name($stmt, ':accountID', $accountID);
+        oci_execute($stmt);
+        $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);
+        $kundenID = $row['KundenID'];
+
+        //KundenID in Session speichern
+        session_start();
+        $_SESSION['userID'] = $kundenID;
     }
 }
-
-
-/**
- * It destroys the session.
- * 
- */
-function logout() {
-    session_destroy();
-}
-
-
-/**
- * It takes a connection, an email and a password and inserts a new account, customer and saves the
- * customerID in the session.
- * 
- * @param email test@test.de
- * @param password the password the user entered
- */
-function register( $email, $password){
-    $passwordHash = md5($password);
-    //Account anlegen
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "INSERT INTO Account (PasswortHash,  EMail, AccountTyp, LetzterLogin, Aktiv) VALUES (:passwordHash, :email, 'Kunde', SYSDATE, '1')");
-    oci_bind_by_name($stmt, ':passwordHash', $passwordHash);
-    oci_bind_by_name($stmt, ':email', $email);
-    oci_execute($stmt);
-
-    //AccountID ermitteln
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT AccountID FROM Account WHERE EMail = :email"); 
-    oci_bind_by_name($stmt, ':email', $email);
-    oci_execute($stmt);
-    $row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS);
-    $accountID = $row['AccountID'];
-
-    //Kunde anlegen
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "INSERT INTO Kunde (AccountID) VALUES (:accountID)");
-    oci_bind_by_name($stmt, ':accountID', $accountID);
-    oci_execute($stmt);
-
-    //KundenID ermitteln
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT KundenID FROM Kunde WHERE AccountID = :accountID");
-    oci_bind_by_name($stmt, ':accountID', $accountID);
-    oci_execute($stmt);
-    $row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS);
-    $kundenID = $row['KundenID'];
-
-    //KundenID in Session speichern
-    session_start();
-    $_SESSION['userID'] = $kundenID;
-}
-
-?>
--- Webseite/php/WarenkorbFunctions.php
+++ Webseite/php/WarenkorbFunctions.php
@@ -1,93 +1,101 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "/db2-buecherwurm/Webseite/php/databaseConnection.php" ;
+require_once $_SERVER["DOCUMENT_ROOT"] . "/db2-buecherwurm/Webseite/php/databaseConnection.php";
 
 
-/**
- * It checks if there's a Warenkorb for the given KundenID, if there is none, it creates one, if there
- * is one, it returns it.
- * 
- * 
- * @return The ID of the Warenkorb
- */
-function getOrCreateWarenkorb(){
-    $kundenID = $_SESSION['userID'];
-    $NumberOfOrders = getNumberOfOrdersInCreation(DatabaseConnection::getDatabaseConnection(), $kundenID);
-    if($NumberOfOrders == 0){
-        createWarenkorb(DatabaseConnection::getDatabaseConnection(), $kundenID);
-        return getWarenkorb(DatabaseConnection::getDatabaseConnection(), $kundenID);
+class WarenkorbFunctions
+{
+
+
+
+    /**
+     * It checks if there's a Warenkorb for the given KundenID, if there is none, it creates one, if there
+     * is one, it returns it.
+     * 
+     * 
+     * @return The ID of the Warenkorb
+     */
+    static function getOrCreateWarenkorb()
+    {
+        $kundenID = $_SESSION['userID'];
+        $NumberOfOrders = WarenkorbFunctions::getNumberOfOrdersInCreation(DatabaseConnection::getDatabaseConnection(), $kundenID);
+        if ($NumberOfOrders == 0) {
+            WarenkorbFunctions::createWarenkorb(DatabaseConnection::getDatabaseConnection(), $kundenID);
+            return WarenkorbFunctions::getWarenkorb(DatabaseConnection::getDatabaseConnection(), $kundenID);
+        } elseif ($NumberOfOrders == 1) {
+            return WarenkorbFunctions::getWarenkorb(DatabaseConnection::getDatabaseConnection(), $kundenID);
+        } else {
+            throw new Exception("FEHLER: Es gibt mehr als einen Warenkorb für diesen Kunden");
+        }
     }
-    elseif($NumberOfOrders == 1){
-        return getWarenkorb(DatabaseConnection::getDatabaseConnection(), $kundenID);
+
+
+    /**
+     * It returns the number of orders in creation for a given customer.
+     * 
+     * 
+     * @return The number of orders in creation.
+     */
+    private static function getNumberOfOrdersInCreation()
+    {
+        $kundeID = $_SESSION['userID'];
+        $sql = "SELECT COUNT(*) AS anzahl FROM bestellung WHERE kundenid = :kundeID AND status = 'wird erstellt'";
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), $sql);
+        oci_bind_by_name($stmt, ":kundeID", $kundeID);
+        if (oci_execute($stmt)) {
+            $row = oci_fetch_assoc($stmt);
+            return $row['ANZAHL'];
+        } else {
+            $error = ocierror($stmt);
+            throw new Exception($error['message']);
+        }
     }
-    else{
-        throw new Exception("FEHLER: Es gibt mehr als einen Warenkorb für diesen Kunden");
+
+
+
+    /**
+     * It returns the ID of the current order of the user.
+     * 
+     * 
+     * @return The ID of the current order.
+     */
+    private static function getWarenkorb()
+    {
+        $userID = $_SESSION['userID'];
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT BestellungID FROM Bestellung WHERE KundenID = :userID AND Status = 'wird erstellt'");
+        oci_bind_by_name($stmt, ':userID', $userID);
+        oci_execute($stmt);
+        $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);
+        $id = $row['ID'];
+        return $id;
     }
-}
 
 
-/**
- * It returns the number of orders in creation for a given customer.
- * 
- * 
- * @return The number of orders in creation.
- */
-function getNumberOfOrdersInCreation() {
-    $kundeID = $_SESSION['userID'];
-    $sql = "SELECT COUNT(*) AS anzahl FROM bestellung WHERE kundenid = :kundeID AND status = 'wird erstellt'";
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), $sql);
-    oci_bind_by_name($stmt, ":kundeID", $kundeID);
-    if(oci_execute($stmt)){
-        $row = oci_fetch_assoc($stmt);
-        return $row['ANZAHL'];
-    }else{
-        $error = ocierror($stmt);
-        throw new Exception($error['message']);
+
+    /**
+     * It inserts a new row into the table Bestellung with the current date, the status "wird erstellt" and
+     * the userID.
+     * 
+     */
+    private static function createWarenkorb()
+    {
+        $userID = $_SESSION['userID'];
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "INSERT INTO Bestellung (Datum, Status, KundenID) VALUES (SYSDATE, 'wird erstellt', :userID)");
+        oci_bind_by_name($stmt, ':userID', $userID);
+        oci_execute($stmt);
     }
-}
 
 
-
-/**
- * It returns the ID of the current order of the user.
- * 
- * 
- * @return The ID of the current order.
- */
-function getWarenkorb() {
-    $userID = $_SESSION['userID'];
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT BestellungID FROM Bestellung WHERE KundenID = :userID AND Status = 'wird erstellt'"); 
-    oci_bind_by_name($stmt, ':userID', $userID);
-    oci_execute($stmt);
-    $row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS);
-    $id= $row['ID'];
-    return $id;
-}
-
-
-
-/**
- * It inserts a new row into the table Bestellung with the current date, the status "wird erstellt" and
- * the userID.
- * 
- */
-function createWarenkorb() {
-    $userID = $_SESSION['userID'];
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "INSERT INTO Bestellung (Datum, Status, KundenID) VALUES (SYSDATE, 'wird erstellt', :userID)");
-    oci_bind_by_name($stmt, ':userID', $userID);
-    oci_execute($stmt);
-}
-
-
-/**
- * It returns an array of all items in the shopping cart.
- * 
- * @param BestellungID The ID of the order
- * 
- * @return Array of items in the shopping cart
- */
-function getWarenkorbItems($BestellungID) {
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT BestellpositionID,
+    /**
+     * It returns an array of all items in the shopping cart.
+     * 
+     * @param BestellungID The ID of the order
+     * 
+     * @return Array of items in the shopping cart
+     */
+    static function getWarenkorbItems($BestellungID)
+    {
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT BestellpositionID,
                                     Titel,
                                     Verlag,
                                     ArtikelID,
@@ -98,92 +106,96 @@ function getWarenkorbItems($BestellungID) {
                             FROM Bestellposition
                             JOIN Artikel on Artikel.ArtikelID = Bestellposition.ArtikelID
                             WHERE BestellungID = :BestellungID");
-    oci_bind_by_name($stmt, ':BestellungID', $BestellungID);
-    oci_execute($stmt);
-    $rows = array();
-    while($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) {
-        $rows[] = $row;
+        oci_bind_by_name($stmt, ':BestellungID', $BestellungID);
+        oci_execute($stmt);
+        $rows = array();
+        while ($row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS)) {
+            $rows[] = $row;
+        }
+        return $rows;
     }
-    return $rows;
+
+
+    /**
+     * It returns the total price of a shopping cart.
+     * 
+     * @param BestellungID The ID of the order
+     * 
+     * @return The total price of the order.
+     */
+    static function getWarenkorbTotal($BestellungID)
+    {
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT Gesamtpreis FROM Bestellung WHERE BestellungID = :BestellungID");
+        oci_bind_by_name($stmt, ':BestellungID', $BestellungID);
+        oci_execute($stmt);
+        $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);
+        return $row['GESAMTPREIS'];
+    }
+
+
+    /**
+     * It inserts a new row into the table Bestellposition.
+     * 
+     * @param BestellungID The ID of the order
+     * @param ArtikelID The ID of the article to be added to the order
+     * @param Menge The amount of the item that the user wants to buy
+     */
+    static function addItemToWarenkorb($BestellungID, $ArtikelID, $Menge)
+    {
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "INSERT INTO Bestellposition (BestellungID, ArtikelID, Menge) VALUES (:BestellungID, :ArtikelID, :Menge)");
+        oci_bind_by_name($stmt, ':BestellungID', $BestellungID);
+        oci_bind_by_name($stmt, ':ArtikelID', $ArtikelID);
+        oci_bind_by_name($stmt, ':Menge', $Menge);
+        oci_execute($stmt);
+    }
+
+
+
+    /**
+     * It deletes a row from the table Bestellposition where the BestellpositionID is equal to the given
+     * BestellpositionID.
+     * 
+     * @param BestellpositionID The ID of the item in the shopping cart that should be deleted
+     */
+    static function deleteItemFromWarenkorb($BestellpositionID)
+    {
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "DELETE FROM Bestellposition WHERE BestellpositionID = :BestellpositionID");
+        oci_bind_by_name($stmt, ':BestellpositionID', $BestellpositionID);
+        oci_execute($stmt);
+    }
+
+
+
+    /**
+     * It updates the amount of a product in the shopping cart.
+     * 
+     * @param BestellpositionID The ID of the item in the shopping cart
+     * @param Menge The new amount of the item that the user wants to buy.
+     */
+    static function updateItemInWarenkorb($BestellpositionID, $Menge)
+    {
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "UPDATE Bestellposition SET Menge = :Menge WHERE BestellpositionID = :BestellpositionID");
+        oci_bind_by_name($stmt, ':BestellpositionID', $BestellpositionID);
+        oci_bind_by_name($stmt, ':Menge', $Menge);
+        oci_execute($stmt);
+    }
+
+
+
+    /**
+     * It updates the status of the order to "offen" and sets the RechnungsadresseID and
+     * LieferadresseID to the given values.
+     * 
+     * @param BestellungID The ID of the order
+     * @param RechnungsadresseID The ID of the billing address
+     * @param LieferadresseID 1
+     */
+    static function sendOrder($BestellungID, $RechnungsadresseID, $LieferadresseID)
+    {
+        $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "UPDATE Bestellung SET Status = 'offen', RechnungsadresseID = :RechnungsadresseID, LieferadresseID = :LieferadresseID WHERE BestellungID = :BestellungID");
+        oci_bind_by_name($stmt, ':BestellungID', $BestellungID);
+        oci_bind_by_name($stmt, ':RechnungsadresseID', $RechnungsadresseID);
+        oci_bind_by_name($stmt, ':LieferadresseID', $LieferadresseID);
+        oci_execute($stmt);
+    }
 }
-
-
-/**
- * It returns the total price of a shopping cart.
- * 
- * @param BestellungID The ID of the order
- * 
- * @return The total price of the order.
- */
-function getWarenkorbTotal( $BestellungID) {
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "SELECT Gesamtpreis FROM Bestellung WHERE BestellungID = :BestellungID");
-    oci_bind_by_name($stmt, ':BestellungID', $BestellungID);
-    oci_execute($stmt);
-    $row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS);
-    return $row['GESAMTPREIS'];
-}
-
-
-/**
- * It inserts a new row into the table Bestellposition.
- * 
- * @param BestellungID The ID of the order
- * @param ArtikelID The ID of the article to be added to the order
- * @param Menge The amount of the item that the user wants to buy
- */
-function addItemToWarenkorb( $BestellungID, $ArtikelID, $Menge) {
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "INSERT INTO Bestellposition (BestellungID, ArtikelID, Menge) VALUES (:BestellungID, :ArtikelID, :Menge)");
-    oci_bind_by_name($stmt, ':BestellungID', $BestellungID);
-    oci_bind_by_name($stmt, ':ArtikelID', $ArtikelID);
-    oci_bind_by_name($stmt, ':Menge', $Menge);
-    oci_execute($stmt);
-}
-
-
-
-/**
- * It deletes a row from the table Bestellposition where the BestellpositionID is equal to the given
- * BestellpositionID.
- * 
- * @param BestellpositionID The ID of the item in the shopping cart that should be deleted
- */
-function deleteItemFromWarenkorb($BestellpositionID) {
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "DELETE FROM Bestellposition WHERE BestellpositionID = :BestellpositionID");
-    oci_bind_by_name($stmt, ':BestellpositionID', $BestellpositionID);
-    oci_execute($stmt);
-}
-
-
-
-/**
- * It updates the amount of a product in the shopping cart.
- * 
- * @param BestellpositionID The ID of the item in the shopping cart
- * @param Menge The new amount of the item that the user wants to buy.
- */
-function updateItemInWarenkorb($BestellpositionID, $Menge) {
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "UPDATE Bestellposition SET Menge = :Menge WHERE BestellpositionID = :BestellpositionID");
-    oci_bind_by_name($stmt, ':BestellpositionID', $BestellpositionID);
-    oci_bind_by_name($stmt, ':Menge', $Menge);
-    oci_execute($stmt);
-}
-
-
-
-/**
- * It updates the status of the order to "offen" and sets the RechnungsadresseID and
- * LieferadresseID to the given values.
- * 
- * @param BestellungID The ID of the order
- * @param RechnungsadresseID The ID of the billing address
- * @param LieferadresseID 1
- */
-function sendOrder( $BestellungID, $RechnungsadresseID, $LieferadresseID) {
-    $stmt = oci_parse(DatabaseConnection::getDatabaseConnection(), "UPDATE Bestellung SET Status = 'offen', RechnungsadresseID = :RechnungsadresseID, LieferadresseID = :LieferadresseID WHERE BestellungID = :BestellungID");
-    oci_bind_by_name($stmt, ':BestellungID', $BestellungID);
-    oci_bind_by_name($stmt, ':RechnungsadresseID', $RechnungsadresseID);
-    oci_bind_by_name($stmt, ':LieferadresseID', $LieferadresseID);
-    oci_execute($stmt);
-}
-
-?>
