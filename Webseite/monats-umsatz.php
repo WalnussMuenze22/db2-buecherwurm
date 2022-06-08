e0d9cb9f2eb691a702673d755e82e6c64d516f12
Jesaja Storm <jesaja.storm@smail.th-koeln.de>
2022-06-08 18:14:12 +0000

    Update Webseite/monats-umsatz.php

---
+++ Webseite/monats-umsatz.php
@@ -0,0 +1,16 @@
+<?php
+    $umsatz = 123;
+?>
+
+<!DOCTYPE HTML>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <title>Monatsumsatz</title>
+    </head>
+    <body>
+        <h1>Monatsumsatz</h1>
+        
+        <p>Der Umsatz des letzten Monats beträgt <?php echo $umsatz;?>.</p>
+    </body>
+</html>
